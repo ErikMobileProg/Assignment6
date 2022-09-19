26c75bd53a528dfe581ae1139690ef2dd60ef2e7
erik <erik_g@web.de>
2022-09-19 18:37:13 +0300

    Assignment 6 complete

--- App.js
+++ App.js
@@ -1,20 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, Button, Alert, Image} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [keyword, setKeyword] = useState('');
+  const [data, setData] = useState([]);
+
+  const fetchRepository = () => {
+
+    if(!keyword){
+
+      Alert.alert('Enter an ingredient');
+
+    }else{
+
+      fetch('https://themealdb.com/api/json/v1/1/filter.php?i=' + keyword)
+        .then(response => response.json())
+        .then(data => setData(data.meals))
+        .catch(err => Alert.alert('Error', err));
+
+    }
+
+
+  }
+
+  const listSeparator = () => {
+
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  
+  };
+  
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <FlatList
+        data={data}
+        renderItem={({ item }) => 
+            <><Text style={styles.text}> {item.strMeal} </Text>
+              <Image style={styles.image} source={{ uri: item.strMealThumb }} /></>
+        }
+        ItemSeparatorComponent={listSeparator}
+      />
+
+      <View style={styles.div}>
+
+        <TextInput
+          style={styles.textinput}
+          placeholder='Ingredient'
+          onChangeText={text => setKeyword(text)}
+        />
+
+        <Button
+          title='Find'
+          onPress={fetchRepository}
+        />
+
+      </View>
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  image: {
+    width: 70, height: 70
+  },
+  textinput: {
+    fontSize: 15
+  },
+  div: {
+    alignItems: 'center' 
+  }
+
 });
